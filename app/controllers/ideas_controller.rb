c2d902b830a50d5818f14656f0fb7e81a25f5370
siansrd <sianrobinsondavies@gmail.com>
2017-07-31 11:48:39 +0100

    Create index and show routes for Ideas rendering appropriate json

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,8 +1,18 @@
 class IdeasController < ApplicationController
 
   def index 
-    ideas = {ideas: "ideas"}
-    render json: ideas
+    ideas = Idea.all
+    render json: ideas, :include => [
+      {:user => {:only => :name}},
+      {:category => {:only => :name}}
+    ]
+  end
+
+  def show
+    idea = Idea.find(params[:id])
+    render json: idea.as_json({
+      include: :comments
+    })
   end
 
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,5 @@
 class Idea < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
+  has_many :comments
 end
