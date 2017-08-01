1e14c7226cde2d842b88cfc918d43c44ee41e454
siansrd <sianrobinsondavies@gmail.com>
2017-08-01 14:16:52 +0100

    Add create, update and destroy routes

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -15,4 +15,38 @@ class IdeasController < ApplicationController
     })
   end
 
+  def create
+    idea = Idea.new(idea_params)
+    if idea.save
+      render json: idea
+    else
+      render json: { status: :create_failed }
+    end
+  end
+
+  def update
+    idea = Idea.find(params[:id]) 
+    if idea.update_attributes(idea_params)
+      render :json => idea
+    else
+      render :json => { status: :update_failed }
+    end
+  end
+
+  def destroy
+    idea = Idea.find(params[:id])
+    if idea.destroy!
+      render :json => { status: :success  }
+    else
+      render :json => { status: :delete_failed }
+    end
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit([:title, :description, :summary, :user_id, :category_id, :votes, :views])
+  end
+
+
 end
