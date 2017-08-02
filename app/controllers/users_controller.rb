8efd6c32a09a577e837f3d3f5e6c20794897e5ac
siansrd <sianrobinsondavies@gmail.com>
2017-08-02 11:30:14 +0100

    Only return name and id for user show route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def show 
     user = User.where(:email => params[:id])
-    render json: user
+    render json: user, :only => [:name, :id]
   end
 
 end
