11373b6f61145faccc90a99988676aa542188cec
siansrd <sianrobinsondavies@gmail.com>
2017-08-02 14:14:44 +0100

    Add route and index for categories

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   scope "/api" do
     resources :ideas
     resources :users
+    resources :categories
   end 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
