7e89d9893db3d3c9d721c7b11604314ec28404be
siansrd <sianrobinsondavies@gmail.com>
2017-08-03 15:29:58 +0100

    Add resources and index for comments

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       resources :ideas
     end
     resources :categories
+    resources :comments
   end 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
