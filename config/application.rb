f0434b75b6852a7920299f8aa0c2a93869d41d4f
siansrd <sianrobinsondavies@gmail.com>
2017-08-03 22:01:06 +0100

    Add PUT to list of cors actions

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module IdeasPlatform
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options, :delete]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :options, :delete]
       end
     end
     
