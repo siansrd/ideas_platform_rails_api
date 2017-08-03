6a679a1c48df23cdc1adccf6c1949144187b07e0
siansrd <sianrobinsondavies@gmail.com>
2017-08-03 15:34:46 +0100

    Comment post route added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,4 +5,17 @@ class CommentsController < ApplicationController
     render json: comments
   end
 
+  def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      render json: comment
+    else
+      render json: { status: :create_failed }
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit([:idea_id, :user_id, :comment_id, :votes, :text])
+  end
+
 end
